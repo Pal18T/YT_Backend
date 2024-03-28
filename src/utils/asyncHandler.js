d4cef6966ff8de033e872be38eba0189ed2b59fb
Prince Pal <palprince3690@gmail.com>
2024-03-28 22:05:52 +0530

    error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+
+//same with try-catch method
+
+// const asyncHandler = (fn) => async (err, req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 404).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
