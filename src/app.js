76d6b0b6be0c996121ac1442f790d8bbb28d47db
Prince Pal <palprince3690@gmail.com>
2024-04-09 13:03:58 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,16 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+//routes  import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)  //placed as prefix of URL
+
+//  http://localhost:8000/api/v1/users/register 
+
+
+
 export { app }
